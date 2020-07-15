6dd959427e5663bf31fbc06d856722c8d4b42f9c
hewei-bit <1003826976@qq.com>
2020-07-15 18:54:37 +0800

    linux_learning

--- linux系统编程/上课/进程与线程/3-1-suid示例.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-int main(void){
-    printf("real uid:%d,real gid:%d\n",getuid(),getgid());
-    printf("effective uid:%d,effective uid:%d\n",geteuid(),getegid());
-    exit(0);
-}
